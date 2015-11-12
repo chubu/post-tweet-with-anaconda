71b4d35cfccd8de15e32c993395c86df5f6066b8
chubu <kentaro.enomoto@syoya.com>
2015-11-12 17:41:32 +0900

    fix bug and refactor.
    fix `import "path/filepath"`.
    make filename constant.
    fix Token struct.
    fix `yaml` variable to `yml`.
    fix getting values of structs.

--- post-tweet-with-anaconda.go
+++ post-tweet-with-anaconda.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"filepath"
 	"github.com/ChimeraCoder/anaconda"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"math/rand"
+	"path/filepath"
 	"time"
 )
 
+const (
+	TOKEN_YAML = "./yaml/token.yml"
+	DATA_YAML  = "./yaml/data.yml"
+)
+
 type Token struct {
-	Consumer, AccessToken map[string]Options
+	Consumer, Access_token Options
 }
 
 type Options struct {
@@ -23,25 +28,25 @@ type Statuses struct {
 
 func main() {
 	// read yaml
-	filename, _ := filepath.Abs("./yaml/token.yml")
-	yaml, err := ioutil.ReadFile(filename)
+	filename, _ := filepath.Abs(TOKEN_YAML)
+	yml, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	var token Token
-	err = yaml.Unmarshal(yaml, &token)
+	err = yaml.Unmarshal(yml, &token)
 	if err != nil {
 		panic(err)
 	}
 
 	// read statuses
-	filename, _ := filepath.Abs("./yaml/data.yml")
-	yaml, err := ioutil.ReadFile(filename)
+	filename, _ = filepath.Abs(DATA_YAML)
+	yml, err = ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	var statuses Statuses
-	err = yaml.Unmarshal(yaml, &statuses)
+	err = yaml.Unmarshal(yml, &statuses)
 	if err != nil {
 		panic(err)
 	}
@@ -49,11 +54,11 @@ func main() {
 	// create api instance
 	anaconda.SetConsumerKey(token.Consumer.Token)
 	anaconda.SetConsumerSecret(token.Consumer.Secret)
-	api := anaconda.NewTwitterApi(token.AccessToken.Token, token.AccessToken.Secret)
+	api := anaconda.NewTwitterApi(token.Access_token.Token, token.Access_token.Secret)
 
 	// post tweet
 	current := time.Now().Unix()
 	rand.Seed(current)
-	num := rand.Intn(len(statuses))
-	api.PostTweet("今の運勢: "+statuses[num], nil)
+	num := rand.Intn(len(statuses.Data))
+	api.PostTweet("今の運勢: "+statuses.Data[num], nil)
 }
